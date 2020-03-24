b04fdfd9a8e78b0295c418c5f85060adbb70adb8
Rishabh Mehan <darkrishabh@gmail.com>
2020-03-24 01:18:24 -0700

    Adding ESLint and Workflow Job (#2)

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,16 +3,16 @@ import { ApolloServer, gql } from 'apollo-server-express';
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-	type Query {
-		hello: String
-	}
+  type Query {
+    hello: String
+  }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
-	Query: {
-		hello: () => 'Hello world!'
-	}
+  Query: {
+    hello: () => 'Hello world!',
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers, mocks: true });
--- src/tests/integration/server.test.ts
+++ src/tests/integration/server.test.ts
@@ -6,26 +6,26 @@ import app from '../../main/server';
 chai.use(chaiHttp);
 
 describe('Server', () => {
-	it('Returns a 400 for GET /graphql', async () => {
-		const respone = await chai.request(app).get('/graphql');
-		expect(respone.status).to.equal(400);
-	});
-	it('Returns a 404 for GET /', async () => {
-		const respone = await chai.request(app).get('/');
-		expect(respone.status).to.equal(404);
-	});
-	it('Returns a 200 for POST /graphql', async () => {
-		const respone = await chai.request(app).post('/graphql').send({
-			operationName: '',
-			variables: {},
-			query: `{
+  it('Returns a 400 for GET /graphql', async () => {
+    const respone = await chai.request(app).get('/graphql');
+    expect(respone.status).to.equal(400);
+  });
+  it('Returns a 404 for GET /', async () => {
+    const respone = await chai.request(app).get('/');
+    expect(respone.status).to.equal(404);
+  });
+  it('Returns a 200 for POST /graphql', async () => {
+    const respone = await chai.request(app).post('/graphql').send({
+      operationName: '',
+      variables: {},
+      query: `{
                 hello
-              }`
-		});
-		expect(respone.status).to.equal(200);
-	});
-	it('Returns a 500 for POST /graphql with wrong query', async () => {
-		const respone = await chai.request(app).post('/graphql');
-		expect(respone.status).to.equal(500);
-	});
+              }`,
+    });
+    expect(respone.status).to.equal(200);
+  });
+  it('Returns a 500 for POST /graphql with wrong query', async () => {
+    const respone = await chai.request(app).post('/graphql');
+    expect(respone.status).to.equal(500);
+  });
 });
